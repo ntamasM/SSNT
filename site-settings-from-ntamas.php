921734c24ca785b72b95dd8273214a767974314f
Ntamas <manolis.ntamadakis@gmail.com>
2024-08-07 00:10:50 +0300

    version 2.0 fixed 2

--- site-settings-from-ntamas.php
+++ site-settings-from-ntamas.php
@@ -3,7 +3,7 @@
 Plugin Name: Site Settings from Ntamas
 Plugin URI: https://ntamadakis.gr
 Description: A plugin to create custom settings page and manage those settings.
-Version: 2.0
+Version: 1.0
 Author: Ntamas
 Author URI: https://ntamadakis.gr
 License: GPL2
@@ -16,20 +16,20 @@ if (!defined('ABSPATH')) {
 }
 
 // Include necessary files
-require_once plugin_dir_path(__FILE__) . 'includes/admin-menu.php';
-require_once plugin_dir_path(__FILE__) . 'includes/settings-page.php';
-require_once plugin_dir_path(__FILE__) . 'includes/additional-settings-page.php';
-// wp_enqueue_style('ssnt_admin_styles', plugin_dir_url(__FILE__) . 'css/ssnt_admin_styles.css');
-
-add_action('admin_enqueue_scripts', 'ssnt_css_and_js');
+require_once plugin_dir_path(__FILE__) . 'admin/includes/admin-menu.php';
+require_once plugin_dir_path(__FILE__) . 'admin/includes/settings-page.php';
+require_once plugin_dir_path(__FILE__) . 'admin/includes/additional-settings-page.php';
+require_once plugin_dir_path(__FILE__) . 'admin/includes/bricks-builder-integration.php';
 
 function ssnt_css_and_js($hook)
 {
-    $current_screen = get_current_screen();
+    if (isset($_GET['page']) && ($_GET['page'] === 'ssnt-settings' || $_GET['page'] === 'ssnt-additional-settings')) {
+        wp_enqueue_media();
+        wp_enqueue_script('ssnt-admin-script', plugins_url('admin/js/ssnt-admin.js', __FILE__), array('jquery'), null, true);
 
-    if (strpos($current_screen->base, 'ssnt-additional-settings') === false) {
-        return;
-    } else {
-        wp_enqueue_style('ssnt_admin_styles', plugin_dir_url(__FILE__) . 'css/ssnt_admin_settings_styles.css');
+        if ($_GET['page'] === 'ssnt-additional-settings') {
+            wp_enqueue_style('ssnt_admin_styles', plugin_dir_url(__FILE__) . 'admin/css/ssnt_admin_settings_styles.css');
+        }
     }
 }
+add_action('admin_enqueue_scripts', 'ssnt_css_and_js');
